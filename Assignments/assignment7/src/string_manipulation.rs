526864361eddcd98f8d44a05573903c0b4cb7a4d
Fixer <77531068+sonukumar-a11@users.noreply.github.com>
2021-03-14 14:40:08 +0530

    Update string_manipulation.rs

--- Assignments/assignment7/src/string_manipulation.rs
+++ Assignments/assignment7/src/string_manipulation.rs
@@ -11,34 +11,34 @@
 /// Returns the String to give the desired output....
 pub fn _desideratum_output(iterator1: &str, iterator2: &str, iterator3: &str) -> String {
     let mut position = 0;
-    let mut i: usize = 0;
+    let mut itration: usize = 0;
     let mut array: Vec<char> = Vec::new();
 
-    while i < iterator2.len() {
+    while itration < iterator2.len() {
         if position % 2 == 0 {
-            let compare_result = if iterator1.chars().nth(i) < iterator2.chars().nth(i) {
-                iterator1.chars().nth(i)
+            let compare_result = if iterator1.chars().nth(itration) < iterator2.chars().nth(itration) {
+                iterator1.chars().nth(itration)
             } else {
-                iterator2.chars().nth(i)
+                iterator2.chars().nth(itration)
             };
-            let result_char = if compare_result < iterator3.chars().nth(i) {
+            let result_char = if compare_result < iterator3.chars().nth(itration) {
                 compare_result
             } else {
-                iterator3.chars().nth(i)
+                iterator3.chars().nth(itration)
             };
             if let Some(_t) = result_char {
                 array.push(result_char.unwrap());
             }
         } else {
-            let compare_result = if iterator1.chars().nth(i) > iterator2.chars().nth(i) {
-                iterator1.chars().nth(i)
+            let compare_result = if iterator1.chars().nth(itration) > iterator2.chars().nth(itration) {
+                iterator1.chars().nth(itration)
             } else {
-                iterator2.chars().nth(i)
+                iterator2.chars().nth(itration)
             };
-            let result_char = if compare_result > iterator3.chars().nth(i) {
+            let result_char = if compare_result > iterator3.chars().nth(itration) {
                 compare_result
             } else {
-                iterator3.chars().nth(i)
+                iterator3.chars().nth(itration)
             };
             if let Some(_t) = result_char {
                 array.push(result_char.unwrap());
